516d28fe6d1697a2529e061ed8cad027e413d262
macrzen <oivxlxvio@gmail.com>
2017-03-09 00:12:29 -0500

    Updated car

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,14 +1,10 @@
 package main.java;
 
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 
 /**
  * TODO: Modify calculation of time.
@@ -23,15 +19,6 @@ public class Car extends Rectangle {
      */
     private int engine, suspension, boost, weight;
 
-    private double speed = 30;
-
-    public void setSpeed(int x, int y) {
-        if (x == -1) this.setX(this.getX()-speed);
-        if (x == 1) this.setX(this.getX() + speed);
-        if (y == -1) this.setY(this.getY() - speed);
-        if (y == 1) this.setY(this.getY() + speed);
-    }
-
     /** TODO: Turbo boost?? */
     private boolean isBoosted;
 
@@ -42,32 +29,35 @@ public class Car extends Rectangle {
     /** Reference to the turn order and identification. TODO: Associate a car to a users name ?? */
     private int id;
 
-    private boolean isFinished;
-
+    /**
+     * Creates a car.
+     * @param x The horizontal location
+     * @param y The vertical location
+     * @param offset The sizing relative to locations sizing
+     * @param id The identifier
+     */
     public Car(double x, double y, double offset, int id) {
         super(x, y, offset, offset);
         this.id = id;
-
         String[] names = { "bug", "blue", "black", "yellow","red"};
         URL resource = getClass().getResource("/main/resources/images/" + names[id % names.length] + ".png");
         this.setFill(new ImagePattern(new Image(resource.toString())));
-
-        // Alternative fill, replace above setFill with the one below, and uncomment the above colors array
-        // Color[] colors = { Color.CYAN, Color.PURPLE, Color.ORANGE, Color.YELLOW, Color.BLACK};
-        // this.setFill(colors[id%colors.length]);
     }
 
+    /**
+     * @return The current time elapsed.
+     */
     public double getTime() { return time; }
 
+    /**
+     * @return The car's identifier
+     */
     public int getIdentifier() {
         return id;
     }
 
     /*
-     * Moves a car from one location to another.
-     * Updates the time.
-     * TODO: Decide on how to compute time. Currently computed by distance traveled each time * 0.32.
-     * TODO: The below calculation is completely arbitrary.
+     * Moves a car from one location to another. Updates the time.
      * @param location The location to move to.
      */
     public void newLocation(double x, double y, double distanceIn) {
@@ -82,8 +72,7 @@ public class Car extends Rectangle {
      */
     @Override
     public String toString() {
-        // for(Location l : stops) System.out.println(l.getName()); // Useful for debugging
-        return "Car #:" + id + "\tTime traveled: " + time; //+ "\tcurrent location " + currentLocation.getName();
+        return "Car #:" + id + "\tTime traveled: " + time;
     }
 
 
